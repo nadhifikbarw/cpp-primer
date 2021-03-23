a9e04a543fa9e779a416b1c3d4cb2141e3bc5df2
nadhifikbarw <nadhifikbarw@gmail.com>
2021-03-23 16:50:17 +0700

    End of chapter 3

---
+++ ch03/ex_345.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main()
+{
+    constexpr int row_size = 3, col_size = 4;
+    int ia[row_size][col_size] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12}
+    };
+
+    using ia_row = int[col_size];
+    
+    // First Version using for-range
+    std::cout << "First Method" << std::endl;
+    for(const ia_row &row : ia)
+    {
+        std::cout << "{ ";
+        for (const int &el : row) std::cout << el << " "; 
+        std::cout << "}" << std::endl;
+    }
+
+    // Second version using subscript
+    std::cout << "Second Method" << std::endl;
+    for(int i = 0; i < row_size; i++)
+    {
+        std::cout << "{ ";
+        for(int j = 0; j < col_size; j++) std::cout << ia[i][j] << " ";
+        std::cout << "}" << std::endl;
+    }
+
+    // Third version using pointer arithmatic
+    std::cout << "Third Method" << std::endl;
+    for(ia_row *i = ia; i != ia + row_size; i++)
+    {
+        std::cout << "{ ";
+        for(int *j = *i; j != (*i + col_size); j++) std::cout << *j << " ";
+        std::cout << "}" << std::endl;
+    }
+}
